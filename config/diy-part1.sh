9fd9115a182904c424f5e2cec025a676ddebd67d
zgs360 <zgs360@gmail.com>
2025-08-30 17:51:29 +0800

    Update diy-part1.sh

--- config/diy-part1.sh
+++ config/diy-part1.sh
@@ -1,4 +1,4 @@
 # 添加PassWall2和OpenClash依赖
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git;main' >> feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small.git;main' >> feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
