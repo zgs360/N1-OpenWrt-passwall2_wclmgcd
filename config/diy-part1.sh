c25ebae6e077c68196674293990e058858cc6ded
zgs360 <zgs360@gmail.com>
2025-08-30 13:02:03 +0800

    Update diy-part1.sh

--- config/diy-part1.sh
+++ config/diy-part1.sh
@@ -1,4 +1,14 @@
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# 删除所有现有feed
+rm -f feeds.conf.default
+
+# 创建新的feeds配置
+cat > feeds.conf.default <<EOF
+src-git packages https://github.com/openwrt/packages.git;openwrt-23.05
+src-git luci https://github.com/openwrt/luci.git;openwrt-23.05
+src-git routing https://git.openwrt.org/feed/routing.git;openwrt-23.05
+src-git telephony https://github.com/openwrt/telephony.git;openwrt-23.05
+
+# 添加PassWall2和OpenClash依赖
+src-git kenzo https://github.com/kenzok8/openwrt-packages.git;main
+src-git small https://github.com/kenzok8/small.git;main
+EOF
