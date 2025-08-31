830226edb20c6bd6689b93d8b7f7b5c7ff5b731e
zgs360 <zgs360@gmail.com>
2025-08-31 11:17:43 +0800

    Update diy-part2.sh

--- config/diy-part2.sh
+++ config/diy-part2.sh
@@ -4,36 +4,7 @@
 sed -i 's/TARGET_rockchip/TARGET_rockchip\|\|TARGET_armvirt/g' package/lean/autocore/Makefile
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.3/g' package/base-files/files/bin/config_generate
 
 #修改密码
-sed -i 's/^root:.*:/root:$1$q6Qf.IUu$Bd2tIMFHYYNOsmsIRBwHC0:19650:0:99999:7:::/g' package/base-files/files/etc/shadow
-
-# => alist
-# rm -rf feeds/packages/lang/golang
-# svn export https://github.com/sbwml/packages_lang_golang/branches/20.x feeds/packages/lang/golang
-# git clone https://github.com/sbwml/luci-app-alist package/alist
-
-# => 阿里ddns
-# git clone https://github.com/chenhw2/luci-app-aliddns.git package/luci-app-aliddns
-
-# => dockerman
-# git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
-
-# => openclash
-git clone --depth=1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-
-# => passwall2
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git -b main package/passwall_package
-git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
-
-# git clone https://github.com/douglarek/luci-app-homeproxy package/luci-app-homeproxy
-# git clone https://github.com/pexcn/openwrt-chinadns-ng.git package/chinadns-ng
-svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
-# => 删除默认argon主题
-# rm -rf feeds/luci/themes/luci-theme-argon
-
-#主题
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
+sed -i 's/^root:.*:/root:$1$sKnYlDLr$7fZvROo6z5iKO9FIYGEbf0:18248:0:99999:7:::/g' package/base-files/files/etc/shadow
